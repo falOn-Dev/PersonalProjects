1f39924f5aa7a6d375d11d2e02da8b26139ea18a
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-05-12 15:32:40 -0400

    Stupid controller bug

--- src/main/java/frc/robot/RobotContainer.kt
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -19,14 +19,14 @@ import frc.robot.subsystems.ExampleSubsystem
  * directly reference the (single instance of the) object.
  */
 object RobotContainer {
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private val driverController = CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT)
+
     init {
         configureBindings()
         // Reference the Autos object so that it is initialized, placing the chooser on the dashboard
         Autos
     }
-
-    // Replace with CommandPS4Controller or CommandJoystick if needed
-    private val driverController = CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT)
 
     /**
      * Use this method to define your `trigger->command` mappings. Triggers can be created via the
